ffe0f648f1c32369ce66a96fa672afc186c5b6e0
Sabin Acharya <sabinacharya444@gmail.com>
2022-04-12 15:14:06 +1000

    react components added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar"
+import Main from "./components/Main"
+import React from "react"
+
+export default function App(){
+    return(
+        <div className="container">
+            < Navbar />
+            <Main />
+        </div>
+    )
+}
